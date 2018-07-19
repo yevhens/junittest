c51427ab0ab06a78c29eb90e9f72f9bf3881b0e8
Yevhen <yevhensokyrko@gmail.com>
2018-07-19 14:00:05 +0300

    second commit

---
+++ src/testing/EntityTest.java
@@ -0,0 +1,16 @@
+package testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EntityTest {
+
+    @Test
+    public void changeAge() {
+    }
+
+    @Test
+    public void doesnotlikehotdogs() {
+    }
+}
